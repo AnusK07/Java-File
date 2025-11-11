ce23d2f8aee8b10b1064042be75592a84941db62
Anas kaleem <130245004+AnusK07@users.noreply.github.com>
2025-11-11 05:26:21 +0500

    Update main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java World!");
+        System.out.println("Current Date & Time: " + LocalDateTime.now());
+    }
+}
